a86f170a9a1531e07f23b0203dcc3ea0ad8b479f
RoxaneVovard <rovane.vovard@gmail.com>
2017-05-02 19:33:57 +0200

    modiff index

--- web/index.php
+++ web/index.php
@@ -1,7 +1,20 @@
 <?php
-
 $pdo = new PDO('sqlite:../database.sqlite');
 
+if(isset($_GET["id"])) {
+	$arts = $_GET["id"];
+}
+
+
+if(!empty($arts)){
+	$stmt = $pdo->prepare("DELETE FROM article WHERE  id=:id");
+	$stmt->execute(['id'=>$arts]);
+	$count = $stmt->rowCount();
+	if($count == 0){
+		http_response_code(404);
+		return;
+	}
+}
 
 $stmt = $pdo->prepare('SELECT * FROM article');
 $stmt -> execute();
@@ -17,18 +30,18 @@ $articles = $stmt-> fetchAll(PDO::FETCH_ASSOC);
 </head>
 
 <div class="row">
- 	<?php foreach ($articles as $article) :?>
+ 	<?php foreach ($articles as $art) :?>
 
 	<div class="col-md-6">
- 		<article class="panel panel-default" id="<?php echo $article['id'] ?>">
+ 		<article class="panel panel-default" id="<?php echo $art['id'] ?>">
 
- 			<h2 class="panel-heading"><?php echo $article['title']; ?></h2>
+ 			<h2 class="panel-heading"><?php echo $art['title']; ?></h2>
 			<div class="panel-body">
- 		 		<p><?php echo $article['description'];?></p>
- 		 		<a href="<?php echo $article['link'];?>"><?php echo $article['link'];?></a>
- 		 		<p><?php echo $article['author'];?></p>
+ 		 		<p><?php echo $art['description'];?></p>
+ 		 		<a href="<?php echo $art['link'];?>"><?php echo $art['link'];?></a>
+ 		 		<p><?php echo $art['author'];?></p>
 			</div>
-			<a class="btn" data-id="<?php echo $article['id'];?>" href="?id=<?php echo $article['id'];?>" ><button class=" glyphicon glyphicon-remove"></button></a>
+			<a class="btn" data-id="<?php echo $art['id'];?>" href="?id=<?php echo $art['id'];?>" ><button class=" glyphicon glyphicon-remove"></button></a>
  		 </article>
 	</div>
 
